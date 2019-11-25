f69371b739ee7a0be7b11bd54b406920a00c6e06
vbpiar <37880618+vbpiar@users.noreply.github.com>
2019-11-25 16:03:48 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+function makeTree(array $inputArray, $parentID = 0 ) {
+
+    $outputArray = [];
+
+    foreach ($inputArray as $item) {
+
+        if ($item['parent_id'] == $parentID) {
+
+            $children = makeTree($inputArray,$item['id']);
+
+            if (isset($children)) {
+
+                $item['children'] = $children;
+
+                $outputArray[] = $item;
+            }
+
+        }
+    }
+    return $outputArray;
+}
+
+$test_array = [
+    ['id' => 1, 'name' => 'Jhon', 'parent_id' => 0],
+    ['id' => 2, 'name' => 'Mike', 'parent_id' => 1],
+    ['id' => 3, 'name' => 'Den', 'parent_id' => 1],
+    ['id' => 4, 'name' => 'Ivan', 'parent_id' => 2],
+    ['id' => 5, 'name' => 'Taras', 'parent_id' => 3],
+    ['id' => 6, 'name' => 'Olga', 'parent_id' => 4],
+    ['id' => 7, 'name' => 'Darya', 'parent_id' => 5],
+    ['id' => 8, 'name' => 'Viktoriya', 'parent_id' => 0],
+];
+
+
+
+$tree = makeTree($test_array);
+print_r($tree);
+
